bc1415446022bcc1cecb434e1274cb6ae2a8df64
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-12 13:01:52 -0800

    (debug)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,9 @@ class RoomsController < ApplicationController
 
   def show
 		@messages = Message.all
+		puts "HERE IS @messages PLEASE VIEW"
+		puts @messages
+		puts "END OF @messages"
   end
 
 end
