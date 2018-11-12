5ba74374d2bf948ee90393d8ca5ba54dd1308508
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-12 13:04:08 -0800

    (fix) fixed debug statement

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,7 +3,7 @@ class RoomsController < ApplicationController
   def show
 		@messages = Message.all
 		puts "HERE IS @messages PLEASE VIEW"
-		puts @messages
+		puts "#{@messages}"
 		puts "END OF @messages"
   end
 
