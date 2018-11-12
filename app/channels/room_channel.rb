ff91b0564acab7ff613c0567a5f4617f4d94d1b9
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-12 14:06:04 -0800

    reverted back to original code. chat works in dev

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -10,6 +10,6 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(data)
 		# ActionCable.server.broadcast "room_channel", message: data['message']
-		Message.create! 
+		Message.create! content: data['message']
   end
 end
