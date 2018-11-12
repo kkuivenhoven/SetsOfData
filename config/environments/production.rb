35f308101eb9c45da435aba24277ecbcce556e31
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-12 13:50:28 -0800

    altered line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
    # config.action_cable.url = 'wss://example.com/cable'
    # config.action_cable.url = 'wss://{hostname}/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ["http://setsofdata.herokuapp.com"]
+  config.action_cable.allowed_request_origins = ["https://setsofdata.herokuapp.com/rooms/show"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
