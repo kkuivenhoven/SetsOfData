07e9f2fc5fdccee9f8c9f7896e487262e6624796
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-12 13:10:53 -0800

    (debug) messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,6 +4,7 @@ class RoomsController < ApplicationController
 		@messages = Message.all
 		puts "HERE IS @messages PLEASE VIEW"
 		puts "#{@messages}"
+		puts "COUNT OF MESSAGES: #{@messages.count}"
 		puts "#{@messages.first}"
 		puts "#{@messages.last}"
 		puts "END OF @messages"
