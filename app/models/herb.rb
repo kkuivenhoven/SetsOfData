c08eef1b15097af4251d030aac96701203571e74
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-12 09:02:47 -0800

    (fix) re-adjusted how import csv file

--- app/models/herb.rb
+++ app/models/herb.rb
@@ -1,10 +1,12 @@
 class Herb < ApplicationRecord
 	require 'csv'
 
+=begin
 	def self.import(file)
 		CSV.foreach(Rails.root.join('newHerbs.csv'), headers: true) do |row|
 			Herb.create! row.to_hash
 		end
 	end
+=end
 
 end
