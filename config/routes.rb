e806341fbadd1c99474c3555e11775406d9baaf4
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-13 05:22:54 -0800

    (verify) added google site verification key

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root controller: :static_pages, action: :home
 
+  get '/google0d522795c8df8607.html',
+    to: proc { |env| [200, {}, ["google-site-verification: google0d522795c8df8607.html"]] }
 
 end
