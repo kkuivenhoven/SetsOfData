7f6f737a1f6e7b408bfa8d94025cb88762c3c221
Kendra Kuivenhoven <kendrakuivenhoven@kendras-mbp.attlocal.net>
2018-11-17 14:16:33 -0800

    (feature:chat) hanged to other method of creating messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,12 +2,6 @@ class RoomsController < ApplicationController
 
   def show
 		@messages = Message.all
-		puts "HERE IS @messages PLEASE VIEW"
-		puts "#{@messages}"
-		puts "COUNT OF MESSAGES: #{@messages.count}"
-		puts "#{@messages.first}"
-		puts "#{@messages.last}"
-		puts "END OF @messages"
   end
 
 end
